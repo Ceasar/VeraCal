07e47075481ef92dc963977946669e9db64c4db8
Ceasar Bautista <cbautista2010@gmail.com>
2012-01-14 13:55:41 -0500

    Added task list to calendar json.

--- apps/calendar/views.py
+++ apps/calendar/views.py
@@ -15,10 +15,7 @@ def calendar_read(request):
   assert request.method == "GET"
   assert 'id' in request.GET
   calendar = Calendar.objects.get(id=int(request.GET['id']))
-  # django adds a _state key to objects so we need to copy and delete it first
-  cal_dict = dict(calendar.__dict__)
-  del cal_dict['_state']
-  return HttpResponse(json.dumps(cal_dict))
+  return HttpResponse(json.dumps(calendar.to_json()))
 
 
 def task_create(request):
@@ -31,12 +28,7 @@ def task_read(request):
   assert request.method == 'GET'
   assert 'id' in request.GET
   task = Task.objects.get(id=int(request.GET['id']))
-  # django adds a _state key to objects so we need to copy and delete it first
-  task_dict = dict(task.__dict__)
-  del task_dict['_state']
-  # additionally, datetime isn't serializble, so we need to make it a string
-  task_dict['date'] = str(task_dict['date'])
-  return HttpResponse(json.dumps(task_dict))
+  return HttpResponse(json.dumps(task.to_json()))
 
 
 def task_update(request):
