bf595eb513167e43be57018b90bea9ed43e5882a
Ceasar Bautista <cbautista2010@gmail.com>
2012-01-14 16:35:30 -0500

    Added some documentation to api.py

--- apps/calendar/api.py
+++ apps/calendar/api.py
@@ -1,3 +1,14 @@
+"""
+RESTful API for the Task and Calendar models.
+
+Resources can be accessed at:
+
+http://127.0.0.1:8000/api/resource/?format=json
+http://127.0.0.1:8000/api/resource/1/?format=json
+http://127.0.0.1:8000/api/resource/schema/?format=json
+http://127.0.0.1:8000/api/resource/set/1;3/?format=json
+"""
+
 from tastypie.resources import ModelResource
 from models import Calendar, Task
 
