4e310a1e503d13cf3c806e9bc944de5399a2f75c
Ceasar Bautista <cbautista2010@gmail.com>
2012-01-14 01:28:53 -0500

    Enabled admin site.

--- settings.py
+++ settings.py
@@ -119,10 +119,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'registration',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
+    # 'registration',
+    'django.contrib.admin',
     # 'django.contrib.admindocs',
 )
 
