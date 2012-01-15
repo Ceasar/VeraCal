189f7c7c28bee7a528ae2258ead4b534620deed4
Ceasar Bautista <cbautista2010@gmail.com>
2012-01-15 12:38:54 -0500

    Got form working.

--- apps/calendar/views.py
+++ apps/calendar/views.py
@@ -1,9 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
+from forms import TaskForm
 from models import Calendar, Task
 
 
 def calendar(request):
-  assert request.method == "GET"
-  return render_to_response('calendar.html')
+  if request.method == 'POST':
+    form = TaskForm(request.POST, Task())
+    if form.is_valid():
+      form.save()
+      # create a new empty form
+      form = TaskForm()
+  else:
+    form = TaskForm()
+  return render_to_response('calendar.html', RequestContext(request,
+    {'form': form}))
